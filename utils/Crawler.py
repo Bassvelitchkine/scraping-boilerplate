ba331e8f2ab5ae4bf83918ae61a652952cce5099
Bastiche <bastien.velitchkine@live.fr>
2023-06-04 13:56:40 +0200

    🔥 Removed useless methods in the template

--- utils/Crawler.py
+++ utils/Crawler.py
@@ -9,8 +9,6 @@ from functools import reduce
 
 import requests
 
-from utils.utilities import phone_number_normalizer
-
 
 class Crawler:
     """
@@ -18,119 +16,67 @@ class Crawler:
     """
 
     def __init__(
-        self,
-        parser,
-        input_file,
-        output_file="output.csv",
-        logger="logs.log",
-        crawling_progression="progression.txt",
-        rate_limit=None,
+        self, parser=None, input_file=None, output_folder="./run", rate_limit=None
     ):
         """
         The init method of the Crawler class.
-        Sets attributes, computes header indexes and existing emails and phone numbers.
+        Sets attributes, computes header indexes.
 
         Parameters
         ----------
         parser : Parser
             The custom parser to use for the crawling. It's an instance of the Parser class.
         input_file : str
-            The path to the input file_
-        output_file : str, optional
-            The path to the output file, by default 'output.csv'
-        logger : str, optional
-            The path to the file to log the progress, by default 'logs.log'
-        crawling_progression : str, optional
-            The file where to save the crawler instance after a crawling session,
-            by default 'progression.txt'
-        rate_limit : _type_, optional
+            The path to the input file
+        output_folder : str, optional
+            The path to the output folder, by default './run'
+        rate_limit : int, optional
             A rate limit to slow down the crawling if needed, by default None
         """
-        self.crawling_progression_ = crawling_progression
-        self.index_ = -1
-        self.rate_limit = rate_limit
-        self.parser = parser
+        self.crawling_progression_ = output_folder + "progression.txt"
 
-        self.__set_input_file(input_file)
-        self.__set_output_file(output_file)
-        self.__set_logger(logger)
-
-        # If there's a file to load, we load it
-        self.__load()
-
-    def __load(self):
-        """
-        Loads the crawler instance from the crawling_progression_ file if there's been
-        a previous crawling session.
-        """
         # We look for a crawling_progression_ file
         try:
             # If one is found we load it
-            with open(self.crawling_progression_, "rb") as f:
-                unpickler = pk.Unpickler(f)
+            with open(self.crawling_progression_, "rb") as file:
+                unpickler = pk.Unpickler(file)
                 self.__dict__ = unpickler.load()
-        # Otherwise we set the attributes to their default values
-        # By computing header indexes and existing emails and phone numbers
         except FileNotFoundError:
+            # Otherwise we set the attributes to their default values
+            self.index_ = -1
+            self.rate_limit = rate_limit
+            self.parser = parser
+            self.output_file_ = output_folder + "extracted_data.csv"
+            self.logger_ = output_folder + "logs.log"
             self.header_indexes_ = self.__compute_header_indexes()
-            (
-                self.existing_emails_,
-                self.existing_phone_numbers_,
-            ) = self.__list_existing_phones_and_emails()
+            self.__set_input_file(input_file)
 
     def __set_input_file(self, input_file):
         """
-        Sets the input file_ attribute if the input file_ has the required columns.
+        Sets the input file attribute if the input file has the required columns.
 
         Parameters
         ----------
         input_file : str
-            The path to the input file_
+            The path to the input file
 
         Raises
         ------
         ValueError
-            If the input file_ doesn't have the required columns
+            If the input file_ doesn't have a website column
         """
         with open(input_file, "r", newline="", encoding="utf-8") as file:
             reader = csv.reader(file)
             header = next(reader)
-            has_required_columns = all(
-                column in header for column in ["website", "email", "phone"]
-            )
 
-            if has_required_columns:
+            if "website" in header:
                 self.input_file_ = input_file
             else:
-                raise ValueError(
-                    "You need a 'website', an 'email' and a 'phone' column in your input csv file. Modify your file or try with a different one"
-                )
-
-    def __set_output_file(self, output_file):
-        """
-        Sets the output file_ attribute.
-
-        Parameters
-        ----------
-        output_file : str
-            The path to the output file
-        """
-        self.output_file_ = output_file
-
-    def __set_logger(self, logger):
-        """
-        Sets the logger attribute.
-
-        Parameters
-        ----------
-        logger : str
-            The path to the logger file
-        """
-        self.logger_ = logger
+                raise ValueError("You need a 'website' column in your input csv file.")
 
     def __log(self, website, outcome, print_it=True):
         """
-        Logs the crawling progression in the logger_ file and prints the progression if print_it is True.
+        Logs the crawling progression in the logger file and prints the progression if print_it is True.
 
         Parameters
         ----------
@@ -151,7 +97,7 @@ class Crawler:
 
     def __compute_header_indexes(self):
         """
-        Computes the header indexes of the input file_.
+        Computes the header indexes of the input file.
 
         Returns
         -------
@@ -162,47 +108,6 @@ class Crawler:
             reader = csv.reader(file)
             header = next(reader)
             return {column: header.index(column) for column in header}
-
-    def __list_existing_phones_and_emails(self):
-        """
-        Lists the existing emails and phone numbers from the input file_.
-
-        Returns
-        -------
-        tuple
-            A tuple containing the sets of existing emails and phone numbers
-        """
-        # Open the input CSV file
-        with open(self.input_file_, "r", newline="", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            _ = next(reader)
-
-            # Read existing email and phone numbers from the input file and store them in sets
-            existing_emails = set()
-            existing_phone_numbers = set()
-
-            for row in reader:
-                emails = row[self.header_indexes_["email"]]
-                # If there are emails in the cell
-                if emails:
-                    # split them in a list and remove trailing whitespaces
-                    emails_list = [email.strip().lower() for email in emails.split(",")]
-                    for email in emails_list:
-                        # If the email is not empty, we add it to the set
-                        if email:
-                            existing_emails.add(email)
-
-                phones = row[self.header_indexes_["phone"]]
-                # If there are phone numbers in the cell
-                if phones:
-                    # split them in a list and remove trailing whitespaces
-                    phones_list = [phone.strip().lower() for phone in phones.split(",")]
-                    for phone in phones_list:
-                        # If the phone number is not empty, we add it to the set
-                        if phone:
-                            existing_phone_numbers.add(phone_number_normalizer(phone))
-
-        return existing_emails, existing_phone_numbers
 
     def __process_extracted_data(self, headers, extracted_data):
         """
@@ -238,22 +143,7 @@ class Crawler:
             """
             header = key_val[0]
             data = set(key_val[1])
-
-            if header == "emails":
-                # We update the set of existing emails with the new ones
-                self.existing_emails_.update(data)
-                # We compute the new emails by removing the existing ones from the new ones
-                new_emails = data.difference(self.existing_emails_)
-                accumulator[header] = ", ".join(new_emails)
-            elif header == "phone_numbers":
-                # We update the set of existing phone numbers with the new ones
-                self.existing_phone_numbers_.update(data)
-                # We compute the new phone numbers by removing the existing ones from the new ones
-                new_phone_numbers = data.difference(self.existing_phone_numbers_)
-                accumulator[header] = ", ".join(new_phone_numbers)
-            else:
-                accumulator[header] = ", ".join(data)
-
+            accumulator[header] = ", ".join(data)
             return accumulator
 
         processed_data = dict(reduce(reducer, extracted_data.items(), {}))
@@ -335,6 +225,6 @@ class Crawler:
         """
         Saves the crawling progression in a pickle file.
         """
-        with open(self.crawling_progression_, "wb") as f:
-            pickler = pk.Pickler(f)
+        with open(self.crawling_progression_, "wb") as file:
+            pickler = pk.Pickler(file)
             pickler.dump(self.__dict__)
